fda5a0fc941dda11603a150ebe381ecf02f91395
AlfanWahyudi <alfan.wahyudi98@gmail.com>
2020-03-14 14:51:47 +0700

    menambahakan list

--- index.php
+++ index.php
@@ -8,5 +8,7 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit dignissimos a, magnam, nemo illo quae harum nisi eligendi officia, itaque odio corrupti accusamus accusantium repudiandae culpa eum maiores cumque nihil.</p>
+    <li>one</li>
+    <li>two</li>
 </body>
 </html>
